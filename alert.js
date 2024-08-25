77b664ea5e8f1bad1a9487f9f9a262abfdc127c4
HariKrishnaKumar <93873053+HariKrishnaKumar@users.noreply.github.com>
2024-08-25 22:27:09 +0530

    Add files via upload
    Still developing

---
+++ alert.js
@@ -0,0 +1,4 @@
+function alt()
+{
+    alert("This webpage is only sutable in large screen(desktop or tv)")
+}
---
+++ app.js
@@ -0,0 +1,6 @@
+var typed=new Typed(".auto-type",{
+    strings:["Programmer","Freelancer","PCDoc","Web-developer","Front-End Developer","Back-end Developer","Data Scientiest"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    looped:true
+})
