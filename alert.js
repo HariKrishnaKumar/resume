7c7706c1c35f01bed1ce4970db83db17d4388da5
HariKrishnaKumar <93873053+HariKrishnaKumar@users.noreply.github.com>
2024-09-21 15:44:53 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,4 +1,4 @@
 function alt()
 {
-    alert("This webpage is only sutable in large screen(desktop or tv)")
+    alert("This webpage is only suitable in large screen(desktop or tv) AND STILL UNDER DEVELOPMENT")
 }
